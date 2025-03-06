1a7eb829756f871b550fdff27effb14799ef5d64
Calvin Prewitt <calvin@jaflabs.com>
2025-03-06 16:07:22 -0600

    changes for wasi target

--- llvm/lib/Support/raw_socket_stream.cpp
+++ llvm/lib/Support/raw_socket_stream.cpp
@@ -42,6 +42,7 @@
 
 using namespace llvm;
 
+#if !defined(__wasi__)
 #ifdef _WIN32
 WSABalancer::WSABalancer() {
   WSADATA WsaData;
@@ -339,3 +340,5 @@ ssize_t raw_socket_stream::read(char *Ptr, size_t Size,
   }
   return raw_fd_stream::read(Ptr, Size);
 }
+
+#endif
